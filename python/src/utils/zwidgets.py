1b1bbee21c9af49763702f639a728d81e48891b1
lzhang938 <lzhang938@gmail.com>
2019-05-24 14:55:46 -0400

    Print log file name

--- python/src/utils/zwidgets.py
+++ python/src/utils/zwidgets.py
@@ -1,21 +1,25 @@
 import os
 import logging
 from logging.config import fileConfig
+from logging import FileHandler
 
 print('zwidgets:', os.getcwd())
 print('zwidgets:', __file__)
 
-def make_logger():
+def _make_logger():
     # Assume the configure file is at the same level of this file
     full_path = os.path.realpath(__file__)
     d1 = os.path.dirname(full_path)
     d2 = os.path.dirname(d1)
     log_conf_file = os.path.join(d2, 'conf', 'zlog.ini')
-    print(log_conf_file)
     fileConfig(log_conf_file)
+    print('Loaded log configuration file ' + log_conf_file)
 
     logger = logging.getLogger()
-    logger.debug('often makes a very good meal of %s', 'visiting tourists')
+    for handler in logger.handlers:
+        if isinstance(handler, FileHandler):
+            print('Logging to file {}'.format(handler.baseFilename))
+
     return logger
 
-mylogger = make_logger()
+mylogger = _make_logger()
