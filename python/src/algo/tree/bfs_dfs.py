dfe8482a039f26aa7b7bab51a7989a6fa0d1ce96
lzhang938 <lzhang938@gmail.com>
2019-11-06 22:37:52 -0500

    Added Dijkstra algorithm

--- python/src/algo/tree/bfs_dfs.py
+++ python/src/algo/tree/bfs_dfs.py
@@ -1,6 +1,9 @@
 # From
 # https://www.youtube.com/watch?v=oLtvUWpAnTQ
 # https://www.youtube.com/watch?v=bD8RT0ub--0&t=883s
+# https://www.youtube.com/watch?v=9wV1VxlfBlI: Dijkstra algorithm
+import heapq
+import math
 
 
 def bfs(graph, s):
@@ -31,7 +34,30 @@ def dfs(graph, s):
         print(v)
 
 
-if __name__ == '__main__':
+def dijkstra(graph, s):
+    pqueue = []
+    heapq.heappush(pqueue, (0, s))
+    seen = set()
+    parent = {s: None}  # To memorize the tree
+    distance = {v: math.inf for v in graph.keys()}
+    distance[s] = 0
+
+    while len(pqueue) > 0:
+        dist, v = heapq.heappop(pqueue)
+        seen.add(v)
+
+        adjacents = graph[v].keys()
+        for w in adjacents:
+            if w not in seen:
+                if dist + graph[v][w] < distance[w]:
+                    heapq.heappush(pqueue, (dist + graph[v][w], w))
+                    parent[w] = v
+                    distance[w] = dist + graph[v][w]
+    print(parent)
+    print(distance)
+
+
+def test_bfs():
     graph = {
         'a': ['b', 'c'],
         'b': ['a', 'c', 'd'],
@@ -42,3 +68,20 @@ if __name__ == '__main__':
     }
 
     bfs(graph, 'e')
+
+
+def test_dijkstra():
+    graph = {
+        'a': {'b': 5, 'c': 1},
+        'b': {'a': 5, 'c': 2, 'd': 1},
+        'c': {'a': 1, 'b': 2, 'd': 4, 'e': 8},
+        'd': {'b': 1, 'c': 4, 'e': 3, 'f': 6},
+        'e': {'c': 8, 'd': 3},
+        'f': {'d': 6}
+    }
+
+    dijkstra(graph, 'a')
+
+
+if __name__ == '__main__':
+    test_dijkstra()
